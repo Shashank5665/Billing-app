87e6929c8db635737d6291ddd1fc4fc1b8d177c0
shashlad7 <shashankkr@ganakalabs.com>
2023-12-26 09:14:56 +0530

    added hosted baseurl in frontend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,2 +1,4 @@
 import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:5000/" });
+export const api = axios.create({
+  baseURL: "https://bill-app-backend.onrender.com/",
+});
